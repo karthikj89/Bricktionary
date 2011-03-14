980b26fd7ebc7b9d98cde1c3e82ced3a2ecd936a
Evan Fisher <schleb@gmail.com>
2011-03-14 00:12:43 -0700

    Added OutlineActivity

---
+++ src/edu/berkeley/cs169/bricktionary/OutlineActivity.java
@@ -0,0 +1,14 @@
+package edu.berkeley.cs169.bricktionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OutlineActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.outline);
+	}
+
+}
