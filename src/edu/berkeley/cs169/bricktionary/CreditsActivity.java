2013c05fa0b01ae22413a4ed8a9fd209fe171cc8
Bernadette Le <bernadette.t.le@gmail.com>
2011-04-05 23:39:36 -0700

    added background. fixed up some stuff. added credits page

---
+++ src/edu/berkeley/cs169/bricktionary/CreditsActivity.java
@@ -0,0 +1,14 @@
+package edu.berkeley.cs169.bricktionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreditsActivity extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.credit);
+	}
+
+}
